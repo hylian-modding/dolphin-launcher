3f1a7352ccccd613d9869e800f3a256dde7bf791
Sylvain <sylvain.domange@outlook.fr>
2022-12-06 09:19:13 +0100

    Run ImGui in the main thread

--- src/Dolphin.ts
+++ src/Dolphin.ts
@@ -1,11 +1,12 @@
+import path from 'path';
 import worker_threads from 'worker_threads';
-import { Dolphin, Config, Core, Gui, AddressSpace } from 'dolphin-js';
+import { Dolphin, Core, AddressSpace } from 'dolphin-js';
 import { ImGui } from "ml64tk";
 import * as Common from './Common'
 import { ImGuiApp } from './ImGuiApp';
 
 class ImGuiAppImpl extends ImGuiApp {
-    private toggleImGuiAction!: Gui.Q.Action;
+    private hostWorker!: worker_threads.Worker;
     private mem1View = new ImGui.MemoryEditor();
 
     constructor() {
@@ -17,17 +18,22 @@ class ImGuiAppImpl extends ImGuiApp {
     }
 
     onNewFrame() {
+        if (!Core.isRunningAndStarted() || !this.appWindow.isVisible())
+            return;
+
         const mem1 = AddressSpace.get(AddressSpace.Type.Mem1);
         this.mem1View.drawWindow('Mem1', mem1, mem1.byteLength);
+
+        // new imgui frame
     }
 
     onClose() {
-        this.toggleImGuiAction.checked = false;
+        this.hostWorker.postMessage({ 'msg': 'notifyHideImGui' });
         return !Core.isRunningAndStarted();
     }
 
-    setToggleImGuiAction(a: Gui.Q.Action) {
-        this.toggleImGuiAction = a;
+    setHostWorker(w: worker_threads.Worker) {
+        this.hostWorker = w;
     }
 
     show() {
@@ -49,92 +55,38 @@ if (worker_threads.isMainThread) {
     });
 
     process.on('message', (startInfo: Common.DolphinStartInfo) => {
-        let hostWorker = new worker_threads.Worker(__filename, { workerData: startInfo });
-        let processFrame: NodeJS.Timer;
+        const hostWorker = new worker_threads.Worker(path.join(__dirname, 'DolphinHostThread.js'), { workerData: startInfo });
 
-        hostWorker.on('message', value => {
-            if (value == 'hostGameStarted' && !startInfo.isConfigure) {
-                processFrame = setInterval(() => {
-                    Dolphin.handleFrame(() => {
-                        // new frame
-                    });
-                }, 1);
-                Dolphin.enableFrameHandler(true);
-            }
-        });
+        if (!startInfo.isConfigure) {
+            const app = new ImGuiAppImpl();
+            app.run();
+            app.setHostWorker(hostWorker);
 
-        hostWorker.on('exit', () => {
-            if (!startInfo.isConfigure)
-                clearInterval(processFrame);
-        });
+            let processFrame: NodeJS.Timer;
+
+            hostWorker.on('message', value => {
+                if (value.msg == 'hostReady') {
+                    processFrame = setInterval(() => {
+                        Dolphin.handleFrame(() => {
+                            // new frame
+                        });
+                    }, 1);
+                    Dolphin.enableFrameHandler(true);
+                }
+                else if (value.msg == 'toggleImGuiVisibility') {
+                    const checked: boolean = value.data;
+                    if (checked) app.show();
+                    else app.hide();
+                }
+            });
+
+            hostWorker.on('exit', () => {
+                if (!startInfo.isConfigure)
+                    clearInterval(processFrame);
+                app.close();
+            });
+        }
 
         process.removeAllListeners('message');
     });
-}
-else {
-    const startInfo: Common.DolphinStartInfo = worker_threads.workerData;
-
-    let app: ImGuiAppImpl;
-    if (!startInfo.isConfigure) {
-        app = new ImGuiAppImpl();
-        app.run();
-    }
-
-    Dolphin.startup({
-        applicationDisplayName: 'ModLoader64',
-        userDirectoryPath: Common.getDolphinUserDirectoryPath()
-    }, () => {
-        Config.setBool('-MAIN_USE_PANIC_HANDLERS', false);
-        Config.setBool('-Main,Interface.PlayMode', !startInfo.isConfigure);
-        Config.setBool('-Main,Display.RenderToMain', !startInfo.isConfigure);
-        Config.setBool('-Main,Interface.HideFPSInfo', false);
-    });
-
-    const processUI = setInterval(() => {
-        Dolphin.processOne();
-        if (Gui.Application.hasExited()) {
-            clearInterval(processUI);
-            if (!startInfo.isConfigure) {
-                app.close();
-                Dolphin.enableFrameHandler(false);
-            }
-            Dolphin.shutdown();
-            worker_threads.parentPort?.close();
-        }
-    }, 16);
-
-    Gui.MainWindow.setIcon('assets/icon.png');
-    Gui.MainWindow.show();
-    Gui.Settings.setToolBarVisible(startInfo.isConfigure);
-    Gui.Settings.setDebugModeEnabled(false);
-
-    if (!startInfo.isConfigure) {
-        let debugMenu = Gui.MainWindow.getMenuBar().addMenu('Debug');
-        let toggleImGuiAction = debugMenu.addAction('ImGui');
-        toggleImGuiAction.checkable = true;
-        toggleImGuiAction.setToggledCallback((c) => {
-            if (c) app.show();
-            else app.hide();
-        });
-        toggleImGuiAction.checked = false;
-        toggleImGuiAction.setShortcut('Ctrl+I');
-        // @ts-ignore
-        app.setToggleImGuiAction(toggleImGuiAction);
-    }
-
-    let helpMenu = Gui.MainWindow.findMenu('Help');
-    if (helpMenu) {
-        let aboutAction = helpMenu.addAction('About ModLoader64');
-        aboutAction.setTriggeredCallback(() => {
-            Gui.Q.CommonDialogs.about(Gui.MainWindow.asWidget(), 'About ModLoader64',
-                'ModLoader64 is a network capable mod loading system for Nintendo 64 and GameCube games.<br/>' +
-                'Its main purpose is creating online multiplayer mods for various games like Ocarina of Time.<br/>' +
-                '<a href="https://modloader64.com/">Website</a> <a href="https://discord.gg/nHb4fXX">Discord</a>');
-        });
-    }
-
-    if (!startInfo.isConfigure)
-        Gui.MainWindow.startGame(startInfo.gameFilePath!);
-
-    worker_threads.parentPort?.postMessage('hostGameStarted');
 }
